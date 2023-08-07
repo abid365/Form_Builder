37fb91a890ba5e124c8793181f0a6353b81309e5
jawad Bin Azam <abidibnazam@gmail.com>
2023-08-07 18:37:05 +0600

    navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 
+// Navbar item small components
+const NavBarItem = ({ title, classProps }) => {
+  return <li className={`mx-4 cursor-pointer ${classProps}`}>{title}</li>;
+};
+
 const NavBar = () => {
   return (
-    <div>
-      <h1>Navbar </h1>
-    </div>
+    <nav className="border-y-4">
+      <div className="p-4">
+        <h1 className="text-4xl font-bold">Brand</h1>
+      </div>
+      <ul>
+        {["link1", "link2", "link3"].map((item, index) => (
+          <NavBarItem key={index + item} title={item}></NavBarItem>
+        ))}
+      </ul>
+    </nav>
   );
 };
 
