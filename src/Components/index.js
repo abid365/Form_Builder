d0e63d7e73c27c15588e583982933d2c5a0b4afa
jawad Bin Azam <abidibnazam@gmail.com>
2023-08-07 13:19:49 +0600

    project setup done

---
+++ src/Components/index.js
@@ -0,0 +1,5 @@
+export { default as Footer } from "./Footer";
+export { default as FormBuilder } from "./FormBuilder";
+export { default as NavBar } from "./NavBar";
+export { default as Welcome } from "./Welcome";
+export { default as Loader } from "./Loader";
