0433634fa6013afaf46b2eda08eb1f189c38f6a5
jawad Bin Azam <abidibnazam@gmail.com>
2023-08-07 22:42:32 +0600

    welcome + swiper

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,20 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 // Navbar item small components
 const NavBarItem = ({ title, classProps }) => {
-  return <li className={`mx-4 cursor-pointer ${classProps}`}>{title}</li>;
+  return (
+    <li className={`mx-4 cursor-pointer hover:cursor-pointer ${classProps}`}>
+      {title}
+    </li>
+  );
 };
 
 const NavBar = () => {
   return (
-    <nav className="border-y-4">
-      <div className="p-4">
-        <h1 className="text-4xl font-bold">Brand</h1>
+    <nav className="border rounded-lg p-4 mt-4 md:flex md:justify-between md:items-center mx-8">
+      <div className="text-slate-700">
+        <h1 className="text-4xl font-bold">Form Builder</h1>
       </div>
-      <ul>
+      <ul className="md:flex hidden list-none items-center">
         {["link1", "link2", "link3"].map((item, index) => (
           <NavBarItem key={index + item} title={item}></NavBarItem>
         ))}
+        <li className="px-4 py-2 border-2 rounded-lg bg-red-400 text-white">
+          Log in
+        </li>
       </ul>
     </nav>
   );
--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -1,7 +1,25 @@
 import React from "react";
-
+import { LuBird } from "react-icons/lu";
 const Welcome = () => {
-  return <div>Welcome</div>;
+  return (
+    <div className="grid grid-cols-3 gap-4 my-5 mx-8">
+      {/* 1st col */}
+      <div className="border rounded-2xl p-10 text-white min-h-screen bg-[#3b37ff]">
+        <p className="text-xs font-bold tracking-wider text-white mb-28">
+          <LuBird className="inline-block text-white text-lg"></LuBird> CRIFORM
+        </p>
+        <h1 className="text-4xl font-extrabold my-7">
+          Start your<br></br> journey with us
+        </h1>
+        <p className="text-slate-200 w-3/4">
+          Boost Productivity: Craft Tailored Forms Effortlessly React-Powered
+          Form Builder.
+        </p>
+      </div>
+      {/* 2nd col */}
+      <div className="border rounded-lg min-h-screen col-span-2">text</div>
+    </div>
+  );
 };
 
 export default Welcome;
